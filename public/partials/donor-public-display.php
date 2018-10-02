9df8ecbfc818fe9f58bb82208bc810e6fbc2ba7e
Yamen Shahin <yamenshahin@gmail.com>
2018-10-02 13:53:40 +0200

    Remove donor form

--- public/partials/donor-public-display.php
+++ public/partials/donor-public-display.php
@@ -26,14 +26,7 @@ $current_user = wp_get_current_user();
             </div>
             <div class="row">
                 <div class="col">
-                    <form>
-                        <div class="form-group">
-                            <label for="donorID">Please insert your ID</label>
-                            <input type="text" class="form-control" id="donorID" aria-describedby="donorIDHelp" placeholder="Enter your ID">
-                            <small id="donorIDHelp" class="form-text text-muted">We'll never share your information with anyone else.</small>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                    
                 </div>
             </div>
         </div>

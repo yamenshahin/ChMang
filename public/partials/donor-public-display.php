803072d077c1dec4e5e504db7971d9fd5c7bc8ed
Yamen Shahin <yamenshahin@gmail.com>
2018-10-02 09:31:13 +0200

    Add donor form insert ID

--- public/partials/donor-public-display.php
+++ public/partials/donor-public-display.php
@@ -12,6 +12,7 @@
  * @subpackage Chmang/public/partials
  */
 get_header(); 
+$current_user = wp_get_current_user();
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
@@ -20,7 +21,19 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col">
-                <h1>Donor</h1>
+                <h1>Donor <?= $current_user->user_firstname . ' ' . $current_user->user_lastname; ?></h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <form>
+                        <div class="form-group">
+                            <label for="donorID">Please insert your ID</label>
+                            <input type="text" class="form-control" id="donorID" aria-describedby="donorIDHelp" placeholder="Enter your ID">
+                            <small id="donorIDHelp" class="form-text text-muted">We'll never share your information with anyone else.</small>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
